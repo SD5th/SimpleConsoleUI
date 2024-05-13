5568f7cee85ac7a94e51ee0e31945797fbb6728b
SD5th <vlaka@ro.ru>
2024-05-13 21:53:16 +0300

    + Connector, ActiveShell - Wind/FuncShells, Window

--- Windows.h
+++ Windows.h
@@ -4,149 +4,99 @@
 using namespace std;
 
 class Shell;
-class WindowShell;
-class FunctionShell;
 
-class Window;
+class Connector;
 class Function;
-class ActiveWindow;
+class ActiveShell;
 
 
 class Shell
 {
     public:
-        enum ShellType {
-            WindowType,
-            FunctionType
-        };
+        // конструкторы-деструкторы
+        Shell(string, Function*, Function*, Connector*);
+
         // деконструкция
-        virtual ShellType   Get_Type() = 0;
-        virtual Window*     Get_Window() = 0; 
-        virtual Function*   Get_Function() = 0;
+        Function*   Get_EntranceFunction();
+
+        Connector*     Get_ShellConnector(); 
+        Function*   Get_ShellFunction();
         
         // операции
-        virtual void        Set_Window(Window*) = 0; 
-        virtual void        Set_Function(Function*) = 0;
-};
+        void        Set_EntranceFunction(Function*);
 
-class WindowShell : public Shell
-{
+        void        Set_ShellConnector(Connector*); 
+        void        Set_ShellFunction(Function*);
     private:
-        Window* window;
-    public:
-        // конструкторы-деструкторы
-        WindowShell(Window*);
-
-        // деконструкция
-        ShellType   Get_Type()      override;
-        Window*     Get_Window()    override; 
-        Function*   Get_Function()  override;
-
-        // операции
-        void        Set_Window(Window*) override; 
-        void        Set_Function(Function*) override;
-};
-
-class FunctionShell : public Shell
-{
-    private:
-        Function* function;
-        int number;     
-    public:
-        // конструкторы-деструкторы
-        FunctionShell(Function*);
-
-        //деконструкция
-        ShellType   Get_Type()      override;
-        Window*     Get_Window()    override; 
-        Function*   Get_Function()  override;
-
-        //операции
-        void        Set_Window(Window*) override; 
-        void        Set_Function(Function*) override;
+        string shellName;
+        Function* entranceFunction;
+        Function* shellFunction;
+        Connector* shellConncetor;
 };
 
 
-
-class Window
+class Connector
 {
     private:
-        string windowName;                         // Имя окна
-        string welcomeMessage;                     // Сообщение при переходе на окно
-
         DynamicArray<Shell*>* shells;
 
     public:
         // конструкторы
-        Window();
-        Window(string, string);
-        ~Window();
+        Connector();
+        Connector(DynamicArray<Shell*>*);
+        ~Connector();
 
         // декомпозиция
-        string                      Get_WindowName();
-        string                      Get_WelcomeMessage();
-
         DynamicArray<Shell*>*       Get_Shells();
-        int                         Get_Amount_Shells();
 
         // операции
-        void Change_WindowName(string);
-        void Change_WelcomeMessage(string);
+        void Set_Shells(DynamicArray<Shell*>*);
         
-        void Append_WindowShell(Window*);
-        void Insert_WindowShell(Window*, int);
-        void Change_WindowShell(Window*, int);
-
-        void Append_FunctionShell(Function*);
-        void Insert_FunctionShell(Function*, int);
-        void Change_FunctionShell(Function*, int);
+        void Append_Shell(Shell*);
+        void Insert_Shell(Shell*, int);
+        void Change_Shell(Shell*, int);
+        void Delete_Shell(int);
 };
 
 typedef void (*adapter_ptr)(DynamicArray<string>);
 class Function
 {
     private:    
-        string functionName;
         adapter_ptr adapter;
 
     public:
         // конструкторы-деструкторы
-        Function(string, adapter_ptr);  
+        Function(adapter_ptr);  
 
         // деконструкция
-        string Get_FunctionName();
         adapter_ptr Get_Adapter();
 
         // операции
-        void Set_FunctionName(string);
         void Set_Adapter(adapter_ptr);
-        
+        void RunFunction(DynamicArray<string>);
 };
 
 
-class ActiveWindow
+class ActiveShell
 {
     private:
-        Window* currentWindow;
-        Window* previousWindow;
-        DynamicArray<Shell*>* shells;
+        Shell* currentShell;
+        Shell* previousShell;
 
     public:
         // конструкторы-деструкторы
-        ActiveWindow(Window* window);
+        ActiveShell(Shell*);
 
         // деконструкция
-        Window* Get_CurrentWindow();
-        Window* Get_PreviousWindow();
-        DynamicArray<Shell*>* Get_Shells();
+        Shell* Get_CurrentShell();
+        Shell* Get_PreviousShell();
     
         //операции
-        void Set_CurrentWindow(Window*);
-        void Set_PreviousWindow(Window*);
-        void Set_Shells(DynamicArray<Shell*>*);
+        void Set_CurrentShell(Shell*);
+        void Set_PreviousShell(Shell*);
 
         void InitializeWindow();
-        void MoveToWindow(Window*);
+        void MoveToShell(Shell*);
         void PickOption(int);
         void PrintWindow(); 
 
@@ -155,197 +105,89 @@ class ActiveWindow
 };
 
 
-// WindowShell
+// Connector
     // конструкторы-деструкторы
-        WindowShell::WindowShell(Window* window):
-            window(window)
+        Connector::Connector():
+            shells(new DynamicArray<Shell*>)    
         { }
-    
-    // деконструкция
-        Shell::ShellType    WindowShell::Get_Type() 
+        Connector::Connector(DynamicArray<Shell*>* shells):
+            shells(shells)    
+        { }
+        Connector::~Connector()
         {
-            return ShellType::WindowType;
-        }
-        Window*             WindowShell::Get_Window()
-        {
-            return window;
-        }  
-        Function*           WindowShell::Get_Function()
-        {
-            return nullptr;
-        } 
-    // операции
-        void                WindowShell::Set_Window(Window* newWindow)
-        {
-            window = newWindow;
-        } 
-        void                WindowShell::Set_Function(Function* newFunction)
-        {
-            return;
+            if (shells != nullptr)
+                free(shells);
         }
 
-// FunctionShell
-    // конструкторы
-        FunctionShell::FunctionShell(Function* function):
-            function(function)
-        { }
-    // деконструкция
-        Shell::ShellType    FunctionShell::Get_Type() 
-        {
-            return ShellType::FunctionType;
-        }
-        Window*             FunctionShell::Get_Window()
-        {
-            return nullptr;
-        }  
-        Function*           FunctionShell::Get_Function()
-        {
-            return function;
-        } 
-    // операции
-        void                FunctionShell::Set_Window(Window* newWindow)
-        {
-            return;
-        } 
-        void                FunctionShell::Set_Function(Function* newFunction)
-        {
-            function = newFunction;
-        }
-// Window
-    // конструкторы-деструкторы
-        Window::Window():
-            windowName("Empty windowName"),
-            welcomeMessage("Empty welcomeMessage"),
-            shells(new DynamicArray<Shell*>)    
-        { }
-        Window::Window(string windowName, string welcomeMessage):
-            windowName(windowName),
-            welcomeMessage(welcomeMessage),
-            shells(new DynamicArray<Shell*>)    
-        { }
-        
-        Window::~Window()
-        {        }
 
     // декомпозиция
-        string                          Window::Get_WindowName()
-        {
-            return windowName;
-        }
-        string                          Window::Get_WelcomeMessage()
-        {
-            return welcomeMessage;
-        }
-        
-        DynamicArray<Shell*>*           Window::Get_Shells()
+        DynamicArray<Shell*>*           Connector::Get_Shells()
         {
             return shells;
         }
-        int                             Window::Get_Amount_Shells()
-        {
-            return shells->GetSize();
-        }
-
     // операции
-        void Window::Change_WindowName(string newWindowName)
+        void Connector::Set_Shells(DynamicArray<Shell*>* newShells)
         {
-            windowName = newWindowName;
-        }   
-        void Window::Change_WelcomeMessage(string newWelcomeMessage)
-        {
-            welcomeMessage = newWelcomeMessage;
+            shells = newShells;
         }
 
-        void Window::Append_WindowShell(Window* window)
+        void Connector::Append_Shell(Shell* shell)
         {
-            shells->Append(new WindowShell(window)); 
+            shells->Append(shell); 
         }
-        void Window::Insert_WindowShell(Window* window, int index)
+        void Connector::Insert_Shell(Shell* shell, int index)
         {
-            shells->Insert(new WindowShell(window), index);
+            shells->Insert(shell, index);
         }
-        void Window::Change_WindowShell(Window* window, int index)
+        void Connector::Change_Shell(Shell* shell, int index)
         {
-            delete shells->Get(index);
-            shells->Set(new WindowShell(window), index);
+            shells->Set(shell, index);
         }
-    
-        void Window::Append_FunctionShell(Function* function)
+        void Connector::Delete_Shell(int index)
         {
-            shells->Append(new FunctionShell(function)); 
+            shells->Delete(index);
         }
-        void Window::Insert_FunctionShell(Function* function, int index)
-        {
-            shells->Insert(new FunctionShell(function), index);
-        }
-        void Window::Change_FunctionShell(Function* function, int index)
-        {
-            delete shells->Get(index);
-            shells->Set(new FunctionShell(function), index);
-        }        
+
 
 // Function
     // конструкторы-деструкторы
-        Function::Function(string functionName, adapter_ptr adapter):
-            functionName(functionName),
+        Function::Function(adapter_ptr adapter):
             adapter(adapter)
         { }
 
     // декомпозиция
-        string Function::Get_FunctionName()
-        {
-            return functionName;
-        }
         adapter_ptr Function::Get_Adapter()
         {
             return adapter;
         }
     // операции
-        void Function::Set_FunctionName(string newFunctionName)
-        {
-            functionName = newFunctionName;
-        }
         void Function::Set_Adapter(adapter_ptr newAdapter)
         {
             adapter = newAdapter;
         }
 
-// ActiveWindow
+// ActiveShell
     // конструкторы-деструкторы
-        ActiveWindow::ActiveWindow(Window* window):
-            currentWindow(window),
-            previousWindow(nullptr)
+        ActiveShell::ActiveShell(Shell* shell):
+            currentShell(shell),
+            previousShell(nullptr)
         {  }
 
     // деконструкция
-        Window* ActiveWindow::Get_CurrentWindow()
+        Shell* ActiveShell::Get_CurrentShell()
         {
-            return currentWindow;
+            return currentShell;
         }
-        Window* ActiveWindow::Get_PreviousWindow()
+        Shell* ActiveShell::Get_PreviousShell()
         {
-            return previousWindow;
-        }
-        DynamicArray<Shell*>* ActiveWindow::Get_Shells()
-        {
-            return shells;
+            return previousShell;
         }
     // операции
-        void ActiveWindow::Set_CurrentWindow(Window* newCurrentWindow)
+        void ActiveShell::Set_CurrentShell(Shell* newCurrentShell)
         {
-            currentWindow = newCurrentWindow;
+            currentShell = newCurrentShell;
         }
-        void ActiveWindow::Set_PreviousWindow(Window* newPreviousWindow)
+        void ActiveShell::Set_PreviousShell(Shell* newPreviousShell)
         {
-            previousWindow = newPreviousWindow;
-        }
-        void ActiveWindow::Set_Shells(DynamicArray<Shell*>* newShells)
-        {
-            shells = newShells;
-        }
-
-        void ActiveWindow::MoveToWindow(Window* nextWindow)
-        {
-            previousWindow = currentWindow;
-            currentWindow = nextWindow;
+            previousShell = newPreviousShell;
         }
